b2a8aa232cd8b8f41d1ae679d816e9b0c12cc767
AnamKhan01 <khan22.anam@gmail.com>
2024-07-22 22:23:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import Header from './Components/Header/Header';
+import Home from './Components/Home/Home';
 import './App.css';
-import homepage from './images/homepage3.png'
+import homepage from './images/Homepage.png'
 
 function App() {
     return (
         <div className="app-container">
-            <Header />
+            <Header/>
+            <div className='bg-container'>
             <img src={homepage} alt="Home page" className='bg-img'></img>
-              <div className="container mt-5">
-                <h3>Tasty Delights, Swiftly Arrived!!</h3>
             </div>
+            <Home/>
         </div>
     );
 }
