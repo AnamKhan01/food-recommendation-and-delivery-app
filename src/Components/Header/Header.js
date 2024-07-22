afe5229aaad325a1e0120d9453d0a2fb8b07deea
AnamKhan01 <khan22.anam@gmail.com>
2024-07-22 22:18:14 +0530

    Update Header.js

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,27 +1,30 @@
 import React from 'react';
-import { Navbar, Nav, Container, Button} from 'react-bootstrap';
+import { Navbar, Nav, Button} from 'react-bootstrap';
 import { FaSignInAlt, FaUserPlus } from 'react-icons/fa';
+import { BsCart4 } from "react-icons/bs";
+import logo from './images/Logos.png';
 import './Header.css';
 
 const Header = () => {
     return (
-        <Navbar expand="lg" className='mt-3'>
-            <Container>
-                <Navbar.Brand href="#" className="navbar-brand">FlashFeast</Navbar.Brand>
+        <Navbar expand="lg">
+            <div className='navbar-container'>
+                <Navbar.Brand href="#" className="navbar-brand"><img src={logo} alt="FlashFeast" className='logo'></img></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto">
-                        <Nav.Link href="#home" className='px-4'>Home</Nav.Link>
-                        <Nav.Link href="#features" className='px-4'>Features</Nav.Link>
-                        <Nav.Link href="#pricing" className='px-4'>Pricing</Nav.Link>
-                        <Nav.Link href="#about" className='px-4'>About</Nav.Link>
+                        <Nav.Link href="#home" className='px-5'>Home</Nav.Link>
+                        <Nav.Link href="#features" className='px-5'>Features</Nav.Link>
+                        <Nav.Link href="#shopping" className='px-5'>Shopping</Nav.Link>
+                        <Nav.Link href="#contact" className='px-5'>Contact</Nav.Link>
+                        <Nav.Link href="#about" className='px-5'><BsCart4 className='icon' /></Nav.Link>
                     </Nav>
                     <div className="auth-buttons">
-                        <Button variant="outline-light" className="sign-in-btn"> <FaUserPlus className="me-2" />Sign In</Button>
-                        <Button variant="warning" className="login-btn"><FaSignInAlt className="me-2" />Login</Button>
+                        <Button variant="outline-light" className="sign-in-btn"> <FaUserPlus className="me-2" />SIGN-IN</Button>
+                        <Button variant="warning" className="login-btn"><FaSignInAlt className="me-2" />LOGIN</Button>
                     </div>
                 </Navbar.Collapse>
-            </Container>
+            </div>
         </Navbar>
     );
 };
