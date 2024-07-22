19e0539410a36f62da4827bd55fd4a7ecdced0f1
AnamKhan01 <khan22.anam@gmail.com>
2024-07-22 22:29:53 +0530

    Uploaded Home

---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+    return(
+        <div className="sub-heading">
+            <p>Tasty Delights</p>
+            <p>Swiftly Arrived</p>
+        </div>
+    )
+}
+
+export default Home;
