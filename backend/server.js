2648fb46e4821aea09b59fd3b66970a32a860347
AnanyaShukla029 <98550710+AnanyaShukla029@users.noreply.github.com>
2024-10-26 14:09:47 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,11 +10,21 @@ import "dotenv/config"
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(cors({
-    origin: "https://flashfeast-bay.vercel.app", 
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true
-}));
+// const allowedOrigins = ['http://localhost:5173/', 'https://flashfeast-bay.vercel.app'];
+
+// app.use(cors({
+//     origin: function (origin, callback) {
+//         if (!origin || allowedOrigins.includes(origin)) {
+//             callback(null, true);
+//         } else {
+//             callback(new Error('Not allowed by CORS'));
+//         }
+//     },
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     credentials: true
+// }));
+
+app.use(cors());
 
 app.use(express.json())
 
